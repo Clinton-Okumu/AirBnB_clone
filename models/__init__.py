f5e8e43bf1293cb12d951c67583889c4855fe002
gideonkiplagat <kiplagatgideon00@gmail.com>
2024-02-10 12:30:09 +0300

    still unfinished

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""__init__.pycreate a unique FileStorage instance for your application"""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
+storage.reload()
